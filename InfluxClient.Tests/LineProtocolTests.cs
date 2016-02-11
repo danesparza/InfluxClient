43792bf84d1cd67595ce9c033e9df46daf99104a
marcinsikora <sikora.mk@gmail.com>
2016-02-11 12:09:32 +0100

    Adds equal sign escaping in line protocol.

--- InfluxClient.Tests/LineProtocolTests.cs
+++ InfluxClient.Tests/LineProtocolTests.cs
@@ -50,5 +50,28 @@ namespace InfluxClient.Tests
             //  Assert
             Assert.AreEqual<string>(expectedFormat, retval);
         }
+
+        [TestMethod]
+        [TestCategory("Format")]
+        public void Format_WithTagContainingEqualSign_IsSuccessful()
+        {
+            //  Arrange
+            Measurement m = new Measurement()
+            {
+                Name = "cpu",
+                Timestamp = DateTime.Parse("2015-10-26 13:48Z")
+            };
+            m.AddField("alarm", false).AddField("Message", "Testing messages").AddTag("token","5#4g==");
+
+            string expectedFormat = "cpu,token=5#4g\\=\\= alarm=false,Message=\"Testing messages\" 1445867280000000000";
+            string retval = string.Empty;
+
+            //  Act
+            retval = LineProtocol.Format(m);
+
+            //  Assert
+            Assert.AreEqual<string>(expectedFormat, retval);
+        }
+
     }
 }
